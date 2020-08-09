fb06f1d99211c214e8cf66804efb048e0a596b40
Nikitha Ramu <nkta.1992@gmail.com>
2020-08-09 19:35:28 +0530

    Updates

---
+++ precache-manifest.b28589a5bc2e8715775b9b944d7ceb5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6d81179e55ecb954ee381211b1e790",
+    "url": "/news_manager/index.html"
+  },
+  {
+    "revision": "02fe3b90a0b8e04f0b4c",
+    "url": "/news_manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b965f8452e3087ad7ba",
+    "url": "/news_manager/static/js/2.dde253d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news_manager/static/js/2.dde253d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fe3b90a0b8e04f0b4c",
+    "url": "/news_manager/static/js/main.562f2339.chunk.js"
+  },
+  {
+    "revision": "1e653ff2387a63b39ab2",
+    "url": "/news_manager/static/js/runtime-main.3d1bea60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news_manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
